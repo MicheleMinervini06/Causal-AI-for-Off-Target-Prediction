e1ec296e02c1e761d1ca134a45beb8fb99cb8259
MicheleMinervini06 <miki.minervini@gmail.com>
2026-04-24 15:17:38 +0200

    added packages

--- dag/__init__.py
+++ dag/__init__.py
@@ -1,15 +1,34 @@
 """Biological DAG primitives for CRISPR feature engineering."""
 
 from dag.features import build_features, create_guide_split, load_raw
+from dag.do_calculus import (
+    backdoor_adjustment,
+    build_intervention_dataset,
+    compare_observational_vs_interventional,
+    do_query,
+)
+from dag.independence_tests import (
+    test_conditional_independence,
+    validate_dag_implications,
+)
 from dag.mismatch import classify_mismatch
 from dag.nodes import CRISPRPairFeatures
 from dag.pam import pam_score
+from dag.scm import CRISPRCausalModel, StructuralEquation
 
 __all__ = [
     "build_features",
+    "build_intervention_dataset",
+    "compare_observational_vs_interventional",
     "classify_mismatch",
     "CRISPRPairFeatures",
+    "CRISPRCausalModel",
     "create_guide_split",
+    "do_query",
     "load_raw",
     "pam_score",
+    "test_conditional_independence",
+    "validate_dag_implications",
+    "backdoor_adjustment",
+    "StructuralEquation",
 ]
